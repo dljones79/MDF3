60f32884185c68acf88514af0d62c4273f4bef8c
David Jones <dljones79@fullsail.edu>
2014-09-17 18:13:08 -0500

    Initial Commit for Widget

---
+++ Widget/app/src/main/java/com/fullsail/djones/android/widget/ToDoObject.java
@@ -0,0 +1,13 @@
+//////////////////////////
+// David Jones          //
+// MDF-3 1409           //
+// Week 3 Widgets       //
+// Full Sail University //
+//////////////////////////
+
+package com.fullsail.djones.android.widget;
+
+import java.io.Serializable;
+
+public class ToDoObject implements Serializable {
+}
